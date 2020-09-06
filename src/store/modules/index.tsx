cc442f0427137869563d1873ebe3349afc6f0e22
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-09-06 21:04:53 +0700

    UpIndex

--- src/store/modules/index.tsx
+++ src/store/modules/index.tsx
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
-import TodoReducer from "./Reducers";
+import TodoReducer from "./TodoReducer";
 import { TodoState } from "./TodoTypes";
+import filter from "./FilterReducer";
 
 export interface StoreState {
   TodoReducer: TodoState;
+  filter: string;
 }
 
 export default combineReducers<StoreState>({
   TodoReducer,
+  filter,
 });
