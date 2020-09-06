14df246136152d72ccd92764aa95fd86d051db01
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-09-06 21:03:19 +0700

    Add_ComponentFooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import TodoListContainers from "./container/TodoListContainers";
+import Footer from "./components/Footer";
+
 const App: React.FC = () => {
   return (
     <div>
       <TodoListContainers />
+      <Footer />
     </div>
   );
 };
